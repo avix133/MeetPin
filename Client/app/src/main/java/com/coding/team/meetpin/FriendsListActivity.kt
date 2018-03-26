810e88007caf90852c3715b8328f89d497f82012
elec <bartlomiej.stapor@gmail.com>
2018-03-26 16:50:11 +0200

    Multiple views done. Settings left to implement

---
+++ Client/app/src/main/java/com/coding/team/meetpin/FriendsListActivity.kt
@@ -0,0 +1,26 @@
+package com.coding.team.meetpin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class FriendsListActivity : AppCompatActivity() {
+    lateinit var resultTextView : TextView
+    lateinit var returnButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends_list)
+        resultTextView = findViewById(R.id.resultTextView)
+        returnButton = findViewById(R.id.returnButton)
+
+        returnButton.setOnClickListener(
+                {
+                    val intent : Intent = Intent(applicationContext,MainActivity::class.java)
+                    startActivity(intent)
+                }
+        )
+    }
+}
--- Client/app/src/main/java/com/coding/team/meetpin/MainActivity.kt
+++ Client/app/src/main/java/com/coding/team/meetpin/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.coding.team.meetpin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var mapButton : Button
+    lateinit var friendsButton : Button
+    lateinit var settingsButton : Button
+    lateinit var joey : WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapButton = findViewById(R.id.mapButton)
+        friendsButton = findViewById(R.id.friendsListButton)
+        settingsButton = findViewById(R.id.settingsButton)
+        joey = findViewById(R.id.joey)
+        joey.loadUrl("file:///android_asset/Joey.html")
+
+
+        mapButton.setOnClickListener(
+        {
+            val intent : Intent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(intent)
+        })
+        friendsButton.setOnClickListener(
+        {
+            val intent : Intent = Intent(applicationContext, FriendsListActivity::class.java)
+            startActivity(intent)
+        })
+        settingsButton.setOnClickListener(
+        {
+            val intent : Intent = Intent(applicationContext, MapActivity::class.java)
+            startActivity(intent)
+        })
+
+
     }
+
+
+
+
+
+
+
+
 }
