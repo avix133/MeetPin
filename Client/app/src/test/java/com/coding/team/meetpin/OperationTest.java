eab7dce1da8d7bd65ada6405faa927540182220d
Bartek <bartlomiej.stapor@gmail.com>
2018-03-31 08:57:58 -0400

    Added tests to Client

---
+++ Client/app/src/test/java/com/coding/team/meetpin/OperationTest.java
@@ -0,0 +1,21 @@
+package com.coding.team.meetpin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OperationTest {
+
+    @Test
+    public void add()
+    {
+        Operation operation = new Operation();
+        Assert.assertEquals(8,operation.add(3,5));
+    }
+
+    @Test
+    public void substract()
+    {
+        Operation operation = new Operation();
+        Assert.assertEquals(3,operation.substract(8,5));
+    }
+}
