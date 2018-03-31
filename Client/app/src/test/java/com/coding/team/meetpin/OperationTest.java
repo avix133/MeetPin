67fba70297f35f1e14b2ca52d5368d0f9ece2797
Bartek <bartlomiej.stapor@gmail.com>
2018-03-31 09:01:51 -0400

    Added dependencies

--- Client/app/src/test/java/com/coding/team/meetpin/OperationTest.java
+++ Client/app/src/test/java/com/coding/team/meetpin/OperationTest.java
@@ -2,6 +2,7 @@ package com.coding.team.meetpin;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class OperationTest {
 
@@ -10,6 +11,7 @@ public class OperationTest {
     {
         Operation operation = new Operation();
         Assert.assertEquals(8,operation.add(3,5));
+
     }
 
     @Test
