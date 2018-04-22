2e5be0ff32445e5472ab94e872431f3f14300dd1
elec <bartlomiej.stapor@gmail.com>
2018-04-22 21:53:27 +0200

    Added picking date, displaying date, message box and for future purposes get text button and edittext. GetTextButton takes input of messagebox and displays it in the box on the right. Button not yet implemented

--- Client/app/src/main/kotlin/com/coding/team/meetpin/activities/NewPinActivity.kt
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/NewPinActivity.kt
@@ -1,5 +1,6 @@
 package com.coding.team.meetpin.activities
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
@@ -8,6 +9,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import com.coding.team.meetpin.R
+import kotlinx.android.synthetic.main.activity_new_pin.datePickerDialogBox
 import java.util.*
 import javax.xml.datatype.DatatypeConstants.MONTHS
 
@@ -18,18 +20,30 @@ class NewPinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_pin)
-        messageBox = findViewById(R.id.message)
         pickDateButton = findViewById(R.id.pickDateButton)
+        messageBox = findViewById(R.id.message)
 
 
+        @SuppressLint("SetTextI18n")
         fun pickDate(view : View)
         {
+            val calendar = Calendar.getInstance()
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val month = calendar.get(Calendar.MONTH)
+            val year = calendar.get(Calendar.YEAR)
 
-        }
+            val datePickerDialog = DatePickerDialog(this, android.R.style.Theme_Holo_Dialog, DatePickerDialog.OnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
+                datePickerDialogBox.text = " Chosen Date:\n " + dayOfMonth + "." + monthOfYear + "." + year
+            },year, month, day)
+            datePickerDialog.show()
+            }
+
         pickDateButton.setOnClickListener {
-//            val intent : Intent = Intent(applicationContext, PickDateActivity::class.java)
-//            startActivity(intent)
-
+            pickDate(this.findViewById(android.R.id.content))
         }
+//        messageBox.get
+        }
+
+
     }
-}
+
