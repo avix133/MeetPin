f2e7326b1f4492b467bab56bf22f4e7220ec3fe5
kmuzalewska <klara.muzalewska@gmail.com>
2018-04-22 18:33:08 +0200

    added taking long and lat from dropping not tested yet

--- Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
@@ -1,5 +1,6 @@
 package com.coding.team.meetpin.activities
 
+import android.graphics.Point
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.DragEvent
@@ -18,7 +19,10 @@ import kotlinx.android.synthetic.main.activity_maps.pin
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    var dropX : Float = 0.0f
+    var dropY : Float = 0.0f
+    lateinit var coord : LatLng
+//    Geocoder geocoder = new Geocoder(mContext);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -26,12 +30,18 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         pin.setOnTouchListener( View.OnTouchListener(function = { view, motionEvent ->
-                if (motionEvent.action == MotionEvent.ACTION_DOWN){
-                    var shadowBuilder = View.DragShadowBuilder(view)
-                    view.startDrag(null, shadowBuilder, view, 0)
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                var shadowBuilder = View.DragShadowBuilder(view)
+                view.startDrag(null, shadowBuilder, view, 0)
 
-                    return@OnTouchListener true
-                }else{
+                return@OnTouchListener true
+            } else if (motionEvent.action == MotionEvent.ACTION_UP){
+                dropX = motionEvent.rawX
+                dropY = motionEvent.rawY
+                coord = pointToCoord(dropX.toInt(), dropY.toInt())
+
+                return@OnTouchListener false
+            } else{
                     return@OnTouchListener false
                 }
             })
@@ -46,6 +56,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(cracow).title("Marker in Cracow"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cracow, 15f))
     }
-
+    fun pointToCoord(x: Int,y: Int): LatLng{
+        var projection = mMap.getProjection()
+        return projection.fromScreenLocation(Point(x, y))
+    }
 
 }
