7f5bdd582034b77d07ceffcff9c0e97677e5b321
kmuzalewska <klara.muzalewska@gmail.com>
2018-04-22 17:41:24 +0200

    drag and drop work I don;t know if that implementation allow to take position from the map

--- Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
@@ -2,6 +2,9 @@ package com.coding.team.meetpin.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.DragEvent
+import android.view.MotionEvent
+import android.view.View
 import com.coding.team.meetpin.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -10,6 +13,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.pin
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,6 +25,18 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        pin.setOnTouchListener( View.OnTouchListener(function = { view, motionEvent ->
+                if (motionEvent.action == MotionEvent.ACTION_DOWN){
+                    var shadowBuilder = View.DragShadowBuilder(view)
+                    view.startDrag(null, shadowBuilder, view, 0)
+
+                    return@OnTouchListener true
+                }else{
+                    return@OnTouchListener false
+                }
+            })
+        )
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -30,4 +46,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(cracow).title("Marker in Cracow"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cracow, 15f))
     }
+
+
 }
