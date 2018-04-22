a0d9b103f1370d70be8e18c17cad038ec2cc3927
kmuzalewska <klara.muzalewska@gmail.com>
2018-04-22 16:45:25 +0200

    It don;t work but saving this version

--- Client/app/src/main/kotlin/com/coding/team/meetpin/activities/PinActivity.kt
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/PinActivity.kt
@@ -1,6 +1,7 @@
 import android.app.Activity
 import android.content.ClipData
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.DragEvent
 import android.view.MotionEvent
 import android.view.View
@@ -8,15 +9,29 @@ import android.view.View.OnDragListener
 import com.coding.team.meetpin.R
 
 
-class PinActivity : Activity(), OnDragListener {
+class PinActivity : Activity() , OnDragListener {
     var pin =findViewById<View>(R.id.pin)
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        var pin = findViewById<View>(R.id.pin)
-        pin.setOnDragListener(this)
+        //pin = findViewById<View>(R.id.pin)
+        pin.setOnTouchListener( View.OnTouchListener(function = {view, motionEvent ->
+//             fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+                if (motionEvent.action == MotionEvent.ACTION_DOWN){
+                    var shadowBuilder = View.DragShadowBuilder(view)
+                    view.startDrag(null, shadowBuilder, view, 0)
+                    view.visibility = View.INVISIBLE
+                    return@OnTouchListener true
+                }else{
+                    return@OnTouchListener false
+                }
+            })
+        )
+
     }
+
+
 
     override fun onDrag(v: View, event: DragEvent): Boolean {
         var result = true
@@ -45,5 +60,22 @@ class PinActivity : Activity(), OnDragListener {
         }// do nothing
         return result
     }
-
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_maps)
+//        var listener = View.OnTouchListener(function = {view, motionEvent ->
+//
+//            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+//
+//                view.y = motionEvent.rawY - view.height/2
+//                view.x = motionEvent.rawX - view.width/2
+//            }
+//
+//            true
+//
+//        })
+//        var pin = findViewById<View>(R.id.pin)
+//        pin.setOnTouchListener(listener)
+//
+//}
 }
