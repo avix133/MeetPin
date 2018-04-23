58978c08c93ee41666f5df81546285b12f22df85
kmuzalewska <klara.muzalewska@gmail.com>
2018-04-23 03:36:16 +0200

    converting coordinates into location coordinates are taken in wrong way- don't know how to fix it

--- Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
@@ -1,11 +1,15 @@
 package com.coding.team.meetpin.activities
 
+import android.content.ClipData
 import android.graphics.Point
+import android.location.Address
+import android.location.Geocoder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.DragEvent
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageView
 import com.coding.team.meetpin.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,39 +19,62 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.pin
+import java.util.*
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback { //
 
     private lateinit var mMap: GoogleMap
     var dropX : Float = 0.0f
     var dropY : Float = 0.0f
+    lateinit var shadowBuilder: View.DragShadowBuilder
     lateinit var coord : LatLng
-//    Geocoder geocoder = new Geocoder(mContext);
+    var geocoder = Geocoder(this, Locale.getDefault())
+    lateinit var address : List<Address>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        pin.setOnTouchListener( View.OnTouchListener(function = { view, motionEvent ->
+        pin.setOnTouchListener(View.OnTouchListener(function = { view, motionEvent ->
             if (motionEvent.action == MotionEvent.ACTION_DOWN) {
-                var shadowBuilder = View.DragShadowBuilder(view)
+                shadowBuilder = View.DragShadowBuilder(view)
                 view.startDrag(null, shadowBuilder, view, 0)
 
                 return@OnTouchListener true
-            } else if (motionEvent.action == MotionEvent.ACTION_UP){
-                dropX = motionEvent.rawX
-                dropY = motionEvent.rawY
-                coord = pointToCoord(dropX.toInt(), dropY.toInt())
+            } else {
 
                 return@OnTouchListener false
-            } else{
-                    return@OnTouchListener false
-                }
-            })
+            }
+        })
         )
+        pin.setOnDragListener(View.OnDragListener(function = { view, dragEvent ->
+            if (dragEvent.action==DragEvent.ACTION_DROP){
+                dropX = dragEvent.x
+                dropY = dragEvent.y
+                return@OnDragListener false
 
+            }else if (dragEvent.action==DragEvent.ACTION_DRAG_ENDED){
+//                println("hehe")
+//                var location: IntArray= intArrayOf(0,1)
+
+
+//                dropX = dragEvent.x
+//                dropY = dragEvent.y
+
+                println(dropX.toString() +" " + dropY.toString())
+                coord = pointToCoord(dropX.toInt(), dropY.toInt())
+                address = geocoder.getFromLocation(coord.latitude, coord.longitude,1)
+                println(address.get(0).getAddressLine(0))
+                return@OnDragListener false
+            }else
+
+                return@OnDragListener false
+        })
+        )
     }
+
+
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -56,6 +83,7 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(cracow).title("Marker in Cracow"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cracow, 15f))
     }
+
     fun pointToCoord(x: Int,y: Int): LatLng{
         var projection = mMap.getProjection()
         return projection.fromScreenLocation(Point(x, y))
