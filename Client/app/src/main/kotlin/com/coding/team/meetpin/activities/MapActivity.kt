ae1e7923278f7a4c687939a49ad97e46cca2472a
kmuzalewska <klara.muzalewska@gmail.com>
2018-04-25 18:45:25 +0200

    it work but differently

--- Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/MapActivity.kt
@@ -6,9 +6,12 @@ import android.location.Address
 import android.location.Geocoder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Display
 import android.view.DragEvent
 import android.view.MotionEvent
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import com.coding.team.meetpin.R
 
@@ -16,80 +19,32 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
-import kotlinx.android.synthetic.main.activity_maps.map
-import kotlinx.android.synthetic.main.activity_maps.pin
-import java.util.*
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
 
     private lateinit var mMap: GoogleMap
-    var dropX : Float = 0.0f
-    var dropY : Float = 0.0f
-    lateinit var shadowBuilder: View.DragShadowBuilder
-    lateinit var coord : LatLng
-//    var geocoder = Geocoder(this, Locale.getDefault())
+    var maxY : Int = 0
+    var maxX : Int = 0
+    lateinit var coord:LatLng
     lateinit var address : List<Address>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        pin.setOnTouchListener(View.OnTouchListener(function = { view, motionEvent ->
-            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
-                shadowBuilder = View.DragShadowBuilder(view)
-                view.startDrag(null, shadowBuilder, view, 0)
-
-                return@OnTouchListener true
-            } else {
-                dropX = motionEvent.x
-                dropY =  motionEvent.y
-                return@OnTouchListener false
-            }
-        })
-        )
-        pin.setOnDragListener(View.OnDragListener(function = { view, dragEvent ->
-            if (dragEvent.action == DragEvent.ACTION_DROP) {
-                Toast.makeText(applicationContext, "DropEvent", Toast.LENGTH_SHORT).show()
-//                var array: IntArray = intArrayOf(0,0)
-//                view.getLocationOnScreen(array)
-//                dropX = array.get(0).toFloat()
-//                dropY = array.get(1).toFloat()
-////
-////                println(dropX.toString() +" " + dropY.toString())
-//                coord = mMap.getProjection().fromScreenLocation(Point(dropX.toInt(), dropY.toInt()))
-////                println(coord.latitude.toString() +" " + coord.longitude.toString())
-//                var geocoder = Geocoder(applicationContext)
-//                address = geocoder.getFromLocation((coord.latitude), (coord.longitude), 1)
-//
-//                Toast.makeText(applicationContext, address.get(0).getAddressLine(0), Toast.LENGTH_SHORT).show()
-                return@OnDragListener false
-
-            } else if (dragEvent.action == DragEvent.ACTION_DRAG_ENDED) {
-//                println("hehe")
-//                var location: IntArray= intArrayOf(0,1)
-                Toast.makeText(applicationContext, "DragEndedt", Toast.LENGTH_SHORT).show()
-
-
-//
-//                println(dropX.toString() +" " + dropY.toString())
-                coord = mMap.getProjection().fromScreenLocation(Point(dropX.toInt(), dropY.toInt()))
-                mMap.addMarker(MarkerOptions().position(coord).title("Marker"))
-//                println(coord.latitude.toString() +" " + coord.longitude.toString())
-                var geocoder = Geocoder(applicationContext)
-                address = geocoder.getFromLocation((coord.latitude), (coord.longitude), 1)
-
-                Toast.makeText(applicationContext, address.get(0).getAddressLine(0), Toast.LENGTH_SHORT).show()
-                return@OnDragListener false
-            } else
-
-                return@OnDragListener false
-        })
-        )
-
+        var displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        maxY= displayMetrics.heightPixels
+        maxX = displayMetrics.widthPixels
 
     }
 
@@ -100,17 +55,27 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         val cracow = LatLng(50.06, 19.94)
         mMap.addMarker(MarkerOptions().position(cracow).title("Marker in Cracow"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cracow, 15f))
-        mMap.setOnMapClickListener(GoogleMap.OnMapClickListener(function = { point ->
-            var geocoder = Geocoder(applicationContext)
-            address = geocoder.getFromLocation((point.latitude), (point.longitude),1)
-            Toast.makeText(applicationContext, address.get(0).getAddressLine(0), Toast.LENGTH_SHORT).show()
-        })
-        )
+        mMap.setOnMarkerDragListener(this)
+        coord = mMap.projection.fromScreenLocation(Point(70, maxY-150))
+        mMap.addMarker(MarkerOptions().position(coord).draggable(true))
     }
 
-//    fun pointToCoord(x: Int,y: Int): LatLng{
-//        var projection = mMap.getProjection()
-//        return projection.fromScreenLocation(Point(x, y))
-//    }
+    override fun onMarkerDragStart(p0: Marker?) {
+
+    }
+
+    override fun onMarkerDrag(p0: Marker?) {
+
+    }
+
+    override fun onMarkerDragEnd(p0: Marker?) {
+        mMap.addMarker(MarkerOptions().position(coord).draggable(true))
+        var geocoder = Geocoder(applicationContext)
+        address= geocoder.getFromLocation(p0!!.position.latitude,p0!!.position.longitude,1)
+        Toast.makeText(applicationContext, address.get(0).getAddressLine(0), Toast.LENGTH_SHORT).show()
+    }
+
 
 }
+
+
