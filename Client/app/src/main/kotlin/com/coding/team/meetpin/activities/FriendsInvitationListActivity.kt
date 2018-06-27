029e0d961b5be888f71a3ed7a44b3a731a5b2bb0
avix133 <avix133@gmail.com>
2018-06-27 23:30:10 +0200

    merged friendList

---
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/FriendsInvitationListActivity.kt
@@ -0,0 +1,71 @@
+package com.coding.team.meetpin.activities
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
+import com.coding.team.meetpin.R
+import com.coding.team.meetpin.dao.model.User
+
+class FriendsInvitationListActivity: MenuActivity() {
+    lateinit var returnButton : Button
+    lateinit var friendsButton : Button
+    lateinit var userList : List<User>
+    lateinit var invitationListView : ListView
+    private var invitationList : MutableList<String> = arrayListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invitation_friends_list)
+
+        returnButton = findViewById(R.id.returnButton)
+        friendsButton = findViewById(R.id.friendsListButton)
+        returnButton.setOnClickListener( {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.putExtra("FROM_ACTIVITY", "FriendsListActivity")
+            startActivity(intent)
+        })
+        friendsButton.setOnClickListener( {
+            val intent = Intent(applicationContext, FriendsListActivity::class.java)
+            intent.putExtra("FROM_ACTIVITY", "FriendsInvitationListActivity")
+            startActivity(intent)
+        })
+        userList = listOf(User("Dawid Tracz", "d.tk@gmail.com"), User("Aga Klim", "klk.mun@gmail.com"), User("Ada Ficek", "ada.ficek@gmail.com"),
+                User("Dawid Traczko", "d.sdsdtk@gmail.com"), User("Aga sadsaKlim", "klksads.mun@gmail.com"), User("sdAda Ficek", "adakoko.ficek@gmail.com"),
+                User("Dawid Traczsss", "dxs.tk@xssgmail.com"), User("sssAga Klim", "klk.mun@gmail.cxxom"), User("Ada Ficekxx", "ada.ficekxxx@gmailsss.com"),
+                User("Dawid cxzTraczsss", "dxs.tk@xssgmail.comzcx"), User("sssAga Klimszcs", "klksks.mun@gmail.cxxom"), User("Ada Ficekxzsadsax", "adsksa.ficekxxx@gmailsss.com"))
+
+        for (u in userList){
+            invitationList.add(u.username)
+        }
+        invitationListView = findViewById(R.id.invitationList)
+        val adapter = MyListAdapter(this, R.layout.invitation_friend, invitationList)
+        invitationListView.adapter = adapter
+    }
+
+    private class MyListAdapter(context: Context, var resource: Int, var objects: MutableList<String>) : ArrayAdapter<String>(context, resource, objects) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+            val view : View = layoutInflater.inflate(resource, null)
+            var name: TextView = view.findViewById(R.id.friendName)
+            val button: Button = view.findViewById(R.id.invitationButton)
+
+            name.text = objects[position]
+
+            button.setOnClickListener {
+                Toast.makeText(context, name.text, Toast.LENGTH_LONG).show()
+
+            }
+
+            return view
+        }
+    }
+}
--- Client/app/src/main/kotlin/com/coding/team/meetpin/activities/FriendsListActivity.kt
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/activities/FriendsListActivity.kt
@@ -9,28 +9,50 @@ import android.os.Build
 import android.os.Bundle
 import android.support.annotation.RequiresApi
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ListView
 import com.coding.team.meetpin.R
+import com.coding.team.meetpin.dao.model.User
 
 class FriendsListActivity : MenuActivity() {
-    lateinit var resultTextView : TextView
+
     lateinit var returnButton : Button
+    lateinit var invitationButton : Button
+    lateinit var userList : List<User>
+    lateinit var friendsListView : ListView
+    private var friendsList : MutableList<String> = arrayListOf()
 
     @TargetApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friends_list)
-        resultTextView = findViewById(R.id.resultTextView)
-        returnButton = findViewById(R.id.returnButton)
 
+        returnButton = findViewById(R.id.returnButton)
+        invitationButton = findViewById(R.id.invitationButton)
         returnButton.setOnClickListener(
-                {
-                    val intent = Intent(applicationContext, MainActivity::class.java)
-                    intent.putExtra("FROM_ACTIVITY", "FriendsListActivity")
-                    startActivity(intent)
-                }
+        {
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.putExtra("FROM_ACTIVITY", "FriendsListActivity")
+            startActivity(intent)
+        }
         )
+        invitationButton.setOnClickListener( {
+            val intent = Intent(applicationContext, FriendsInvitationListActivity::class.java)
+            intent.putExtra("FROM_ACTIVITY", "FriendsListActivity")
+            startActivity(intent)
+        })
+        userList = listOf(User("Dawid Tracz", "d.tk@gmail.com"), User("Aga Klim", "klk.mun@gmail.com"), User("Ada Ficek", "ada.ficek@gmail.com"),
+                User("Dawid Traczko", "d.sdsdtk@gmail.com"), User("Aga sadsaKlim", "klksads.mun@gmail.com"), User("sdAda Ficek", "adakoko.ficek@gmail.com"),
+                User("Dawid Traczsss", "dxs.tk@xssgmail.com"), User("sssAga Klim", "klk.mun@gmail.cxxom"), User("Ada Ficekxx", "ada.ficekxxx@gmailsss.com"),
+                User("Dawid cxzTraczsss", "dxs.tk@xssgmail.comzcx"), User("sssAga Klimszcs", "klksks.mun@gmail.cxxom"), User("Ada Ficekxzsadsax", "adsksa.ficekxxx@gmailsss.com"))
+
+        for (u in userList){
+            friendsList.add(u.username)
+        }
+        friendsListView = findViewById(R.id.friendsList)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendsList)
+        friendsListView.adapter = adapter
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
