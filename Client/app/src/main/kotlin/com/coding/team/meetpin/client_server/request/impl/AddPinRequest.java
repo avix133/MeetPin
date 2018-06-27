9e453d447cdc55593db9b2bd5395417c955baaac
Kacper Kowalcze <kacper.kowalcze@student.uj.edu.pl>
2018-06-27 21:15:22 +0200

    Refactor client side

--- Client/app/src/main/kotlin/com/coding/team/meetpin/client_server/request/impl/AddPinRequest.java
+++ Client/app/src/main/kotlin/com/coding/team/meetpin/client_server/request/impl/AddPinRequest.java
@@ -2,13 +2,17 @@ package com.coding.team.meetpin.client_server.request.impl;
 
 import com.coding.team.meetpin.client_server.request.Request;
 import com.coding.team.meetpin.client_server.request.RequestType;
+import com.coding.team.meetpin.dao.model.Pin;
 
 public class AddPinRequest implements Request {
 
+    private static final long serialVersionUID = -6292359922357076064L;
     private int clientId;
+    private Pin pin;
 
-    public AddPinRequest(final int clientId) {
+    public AddPinRequest(final int clientId, Pin pin) {
         this.clientId = clientId;
+        this.pin = pin;
     }
 
     @Override
@@ -19,5 +23,9 @@ public class AddPinRequest implements Request {
     @Override
     public RequestType getType() {
         return RequestType.ADD_PIN;
+    }
+
+    public Pin getPin() {
+        return pin;
     }
 }
