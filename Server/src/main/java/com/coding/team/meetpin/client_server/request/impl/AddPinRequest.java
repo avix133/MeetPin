b5bc30f66342a9adcb909bdf4ec3c8358b921f8d
avix133 <avix133@gmail.com>
2018-07-06 00:57:58 +0200

    faith restored

--- Server/src/main/java/com/coding/team/meetpin/client_server/request/impl/AddPinRequest.java
+++ Server/src/main/java/com/coding/team/meetpin/client_server/request/impl/AddPinRequest.java
@@ -1,18 +1,25 @@
 package com.coding.team.meetpin.client_server.request.impl;
 
+import java.util.List;
+
 import com.coding.team.meetpin.client_server.request.Request;
 import com.coding.team.meetpin.client_server.request.RequestType;
 import com.coding.team.meetpin.dao.model.Pin;
+import com.coding.team.meetpin.dao.model.User;
 
 public class AddPinRequest implements Request {
 
     private static final long serialVersionUID = -6292359922357076064L;
     private int clientId;
     private Pin pin;
+    private boolean global;
+    private List<User> recipients;
 
-    public AddPinRequest(final int clientId, Pin pin) {
+    public AddPinRequest(final int clientId, Pin pin, boolean global, List<User> recipients) {
         this.clientId = clientId;
         this.pin = pin;
+        this.global = global;
+        this.recipients = recipients;
     }
 
     @Override
@@ -27,5 +34,13 @@ public class AddPinRequest implements Request {
 
     public Pin getPin() {
         return pin;
+    }
+
+    public boolean isGlobal() {
+        return global;
+    }
+
+    public List<User> getRecipients() {
+        return recipients;
     }
 }
